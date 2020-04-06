488b0568e56120aee346bc663fb42119f66591f5
Dion Hulse <contact@dd32.id.au>
2020-04-06 21:53:27 +1000

    POC working for basic equal queries, rewrites the SELECT from the postmeta to the cached table.

--- plugin.php
+++ plugin.php
@@ -28,6 +28,8 @@ class xPostMetaCache {
 	protected function __construct() {
 		global $wpdb;
 		$wpdb->xpostmetacache = $wpdb->prefix . 'xpostmetacache';
+
+		add_filter( 'get_meta_sql', [ $this, 'get_meta_sql'], 100, 6 );
 	}
 
 	protected $fields = [];
@@ -39,6 +41,43 @@ class xPostMetaCache {
 
 		return $this;
 	}
+
+	public function get_meta_sql( $sql, $meta_queries, $type, $primary_table, $primary_id_column, $context ) {
+		global $wpdb;
+
+		// POC, we're only going to kick in for a very simple lookup.
+		// Yes, yes, this is not secure and it's horrible.
+
+		// Find the fields we're matching on.
+		preg_match_all( "#\s((\S+)\.meta_key = '(.+?)' AND \S+.meta_value = '(.+)')#", $sql['where'], $matches );
+		$replace = $matches[1];
+		$tables  = $matches[2];
+		$fields  = $matches[3];
+		$values  = $matches[4];
+
+		$expidited_keys = array_keys( $this->fields );
+
+		foreach ( $fields as $i => $f ) {
+			if ( isset( $this->fields[ $f ] ) ) {
+				// Lets do a priority-one delivery on that one.
+				$table_alias = $tables[ $i ];
+				$sql['join'] = str_replace(
+					"INNER JOIN $table_alias ON ( wp_xviews_posts.ID = $table_alias.post_id )",
+					"INNER JOIN $wpdb->xpostmetacache ON ( wp_xviews_posts.ID = $wpdb->xpostmetacache.post_id )",
+					$sql['join']
+				);
+				$sql['where'] = str_replace(
+					$replace[ $i ],
+					"`$wpdb->xpostmetacache`.`$f` = '" . $values[$i] . "'",
+					$sql['where']
+				);
+			}
+		}
+
+		return $sql;
+
+	}
+
 
 	public function create_tables() {
 		global $wpdb;
