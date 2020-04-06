51ae36595a6875d669d99c7ee921bc17a5ec4c01
Dion Hulse <contact@dd32.id.au>
2020-04-06 21:22:50 +1000

    Add the initial cache table, some hacky filling code

--- plugin.php
+++ plugin.php
@@ -26,6 +26,60 @@ class xPostMetaCache {
 	}
 
 	protected function __construct() {
+		global $wpdb;
+		$wpdb->xpostmetacache = $wpdb->prefix . 'xpostmetacache';
+	}
+
+	protected $fields = [];
+	public function register_field( $field, $type = 'varchar(255)', $unique = false ) {
+		$this->fields[ $field ] = [
+			'type'   => $type,
+			'unique' => $unique,
+		];
+
+		return $this;
+	}
+
+	public function create_tables() {
+		global $wpdb;
+		if ( ! $this->fields ) {
+			return;
+		}
+
+		$fields = [];
+		$keys   = [];
+
+		foreach ( $this->fields as $f => $details ) {
+			$fields[] = "`{$f}` {$details['type']}";
+			$keys[]   = ($details['unique'] ? 'UNIQUE ' : '' ) . "KEY `{$f}` (`{$f}`)";
+		}
+
+		$fields = implode( ",\n", $fields );
+		$keys   = implode( ",\n", $keys );
+		$create_sql = "CREATE TABLE `{$wpdb->xpostmetacache}` (
+			`post_id` bigint(20) unsigned NOT NULL,
+			{$fields},
+			PRIMARY KEY (`post_id`),
+			{$keys}
+		  )";
+
+		include_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $create_sql );
+
+		return $this;
+	}
+
+	function fill_table() {
+		global $wpdb;
+
+		foreach ( $this->fields as $f => $data ) {
+			$sql = "INSERT INTO `{$wpdb->xpostmetacache}` (`post_id`, `$f`)
+				SELECT `post_id`, `meta_value` FROM `{$wpdb->postmeta}`
+				WHERE `meta_key` = '$f'
+				ON DUPLICATE KEY UPDATE `$f` = VALUE(`$f`)
+				";
+			$wpdb->query( $sql );
+		}
 	}
 }
 
--- test.php
+++ test.php
@@ -3,5 +3,15 @@ namespace dd32\xPostMetaCache;
 
 add_action( 'init', function() {
 	// Let the fun begin.
+	xPostMetaCache::instance()
+		->setting( 'post_limiter', false )
 
+		->register_field( '_title_id', 'bigint(20) unsigned', true )
+		->register_field( '_wp_page_template' )
+		->register_field( 'test', 'tinyint(3)' )
+	
+		->create_tables()
+
+		->fill_table()
+		;
 });
